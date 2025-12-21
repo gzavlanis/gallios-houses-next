8fe44b714778a11aa3856445e5ead8e76be6d159
George Zavlanis <90002254+gzavlanis@users.noreply.github.com>
2025-12-21 10:53:53 +0200

    middleware correction

--- middleware.js
+++ middleware.js
@@ -25,5 +25,5 @@ export function middleware(request) {
 }
 
 export const config = {
-    matcher: ['/((?!api|_next/static|_next/image|assets|favicon.ico).*)'],
-};
+    matcher: ['/((?!api|_next/static|_next/image|assets|favicon.ico|sitemap.xml|robots.txt).*)']
+}
